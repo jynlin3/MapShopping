8df19481ac56e8a67184c9be4b933bece80b9d1d
Jyn Lin <s871872000@gmail.com>
2022-01-13 16:16:28 -0800

    Reformat code.

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -23,28 +23,32 @@ class Item {
   final bool isDeleted;
   final bool isChecked;
 
-  Item({required this.id, required this.title, required this.isDeleted, required this.isChecked});
+  Item(
+      {required this.id,
+      required this.title,
+      required this.isDeleted,
+      required this.isChecked});
 
-  Item.random(String title, bool isDeleted) :
-    this.id = null,
-    this.title = title,
-    this.isDeleted = isDeleted,
-    this.isChecked = false;
+  Item.random(String title, bool isDeleted)
+      : this.id = null,
+        this.title = title,
+        this.isDeleted = isDeleted,
+        this.isChecked = false;
 
   // Convert a Map into a Item
-  Item.fromDB(Map<String, dynamic> map):
-      id = map[columnId],
-      title = map[columnTitle],
-      isDeleted = map[columnIsDeleted] == 1,
-      isChecked = map[columnIsChecked] == 1;
+  Item.fromDB(Map<String, dynamic> map)
+      : id = map[columnId],
+        title = map[columnTitle],
+        isDeleted = map[columnIsDeleted] == 1,
+        isChecked = map[columnIsChecked] == 1;
 
   // Convert a Item into a Map.
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return {
       columnId: id,
       columnTitle: title,
-      columnIsDeleted: isDeleted ? 1: 0,
-      columnIsChecked: isChecked ? 1: 0
+      columnIsDeleted: isDeleted ? 1 : 0,
+      columnIsChecked: isChecked ? 1 : 0
     };
   }
 }
@@ -59,24 +63,25 @@ class DatabaseHelper {
 
   // make this a singleton class
   DatabaseHelper._privateConstructor();
+
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
   // only have a single app-wide reference to the database
   static Database? _database;
+
   Future<Database?> get database async {
-    if(_database != null) return _database;
+    if (_database != null) return _database;
     // lazily instantiate the db the first time it is accessed
     _database = await _initDatabase();
     return _database;
   }
 
   // open the database (and create it if it does not exist)
-  _initDatabase() async{
+  _initDatabase() async {
     var databasePath = await getDatabasesPath();
     String path = join(databasePath, _databaseName);
     return await openDatabase(path,
-        version: _databaseVersion,
-        onCreate: _onCreate);
+        version: _databaseVersion, onCreate: _onCreate);
   }
 
   // SQL code to create the database table
@@ -110,21 +115,21 @@ class DatabaseHelper {
   }
 
   // Insert/Find/Update/Delete in itemTable
-  Future<int> insertItem(Item item) async{
+  Future<int> insertItem(Item item) async {
     Database? db = await instance.database;
-    return await db!.insert(itemTable, item.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+    return await db!.insert(itemTable, item.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
   Future<List<Item>> getAllItems() async {
     Database? db = await instance.database;
     // final sql = 'SELECT * FROM $itemTable WHERE $columnIsDeleted == 0';
     // final List<Map<String, dynamic>> maps = await db!.rawQuery(sql);
-    final List<Map<String, dynamic>> maps = await db!.query(
-        itemTable,
+    final List<Map<String, dynamic>> maps = await db!.query(itemTable,
         where: '$columnIsDeleted = ?',
         whereArgs: [0],
         orderBy: '$columnIsChecked');
-    return List.generate(maps.length, (i){
+    return List.generate(maps.length, (i) {
       return Item.fromDB(maps[i]);
     });
   }
@@ -141,85 +146,75 @@ class DatabaseHelper {
 
   Future<void> deleteItem(int id) async {
     Database? db = await instance.database;
-    await db!.delete(
-        itemTable,
-        where: 'id = ?',
-        whereArgs: [id]
-    );
+    await db!.delete(itemTable, where: 'id = ?', whereArgs: [id]);
   }
 
   // Insert/Find/Update/Delete in productTable
-  Future<int> insertProduct(Product product, String itemTitle) async{
+  Future<int> insertProduct(Product product, String itemTitle) async {
     Database? db = await instance.database;
     Map<String, dynamic> productMap = product.toMap();
     productMap[columnItemTitle] = itemTitle;
-    return await db!.insert(productTable, productMap, conflictAlgorithm: ConflictAlgorithm.replace);
+    return await db!.insert(productTable, productMap,
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
   Future<List<Product>> getAllProducts() async {
     Database? db = await instance.database;
     final sql = 'SELECT * FROM $productTable WHERE $columnIsDeleted == 0';
     final List<Map<String, dynamic>> maps = await db!.rawQuery(sql);
-    return List.generate(maps.length, (i){
+    return List.generate(maps.length, (i) {
       return Product.fromDB(maps[i]);
     });
   }
 
   Future<void> deleteProductByName(String name) async {
     Database? db = await instance.database;
-    await db!.delete(
-        productTable,
-        where: '$columnTitle = ?',
-        whereArgs: [name]
-    );
+    await db!
+        .delete(productTable, where: '$columnTitle = ?', whereArgs: [name]);
   }
 
   Future<bool> isStoreInProductTable(String store) async {
     Database? db = await instance.database;
-    return (await db!.query(
-        productTable,
-        where: '$columnIsDeleted = ? AND $columnStore = ?',
-        whereArgs: [0, store], limit: 1)).length > 0;
+    return (await db!.query(productTable,
+                where: '$columnIsDeleted = ? AND $columnStore = ?',
+                whereArgs: [0, store],
+                limit: 1))
+            .length >
+        0;
   }
 
-  Future<void> deleteProductsByItemName(String itemName) async{
+  Future<void> deleteProductsByItemName(String itemName) async {
     Database? db = await instance.database;
-    await db!.delete(
-        productTable, where: '$columnItemTitle = ?', whereArgs: [itemName]);
+    await db!.delete(productTable,
+        where: '$columnItemTitle = ?', whereArgs: [itemName]);
   }
 
-  Future<void> updateProductsByItemName(String itemName, bool isTempDeleted) async{
+  Future<void> updateProductsByItemName(
+      String itemName, bool isTempDeleted) async {
     Database? db = await instance.database;
-    await db!.update(
-        productTable,
-        {columnIsDeleted: isTempDeleted},
-        where: '$columnItemTitle = ?',
-        whereArgs: [itemName]
-    );
+    await db!.update(productTable, {columnIsDeleted: isTempDeleted},
+        where: '$columnItemTitle = ?', whereArgs: [itemName]);
   }
 
   // Insert/Find/Update/Delete in storeTable
-  Future<int> insertStore(Place place) async{
+  Future<int> insertStore(Place place) async {
     Database? db = await instance.database;
-    return await db!.insert(
-        storeTable, place.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+    return await db!.insert(storeTable, place.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
   Future<List<Place>> getAllStores() async {
     Database? db = await instance.database;
-    final List<Map<String, dynamic>> maps = await db!.query(
-        storeTable,
-        where: '$columnIsDeleted = ?',
-        whereArgs: [0]
-    );
-    return List.generate(maps.length, (i){
+    final List<Map<String, dynamic>> maps = await db!
+        .query(storeTable, where: '$columnIsDeleted = ?', whereArgs: [0]);
+    return List.generate(maps.length, (i) {
       return Place.fromDB(maps[i]);
     });
   }
 
-  Future<void> deleteStoresByName(String storeName) async{
+  Future<void> deleteStoresByName(String storeName) async {
     Database? db = await instance.database;
-    await db!.delete(storeTable, where: '$columnName LIKE ?', whereArgs: ['$storeName%']);
+    await db!.delete(storeTable,
+        where: '$columnName LIKE ?', whereArgs: ['$storeName%']);
   }
-
 }
